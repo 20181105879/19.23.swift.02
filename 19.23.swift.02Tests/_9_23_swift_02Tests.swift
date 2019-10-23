e434d78e1ab887613dfaa0a629d1df64fc7c03bd
20181105879 <2605753145@qq.com>
2019-10-23 11:00:23 +0800

    Initial Commit

---
+++ 19.23.swift.02Tests/_9_23_swift_02Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _9_23_swift_02Tests.swift
+//  19.23.swift.02Tests
+//
+//  Created by s20181105879 on 2019/10/23.
+//  Copyright © 2019 s20181105879. All rights reserved.
+//
+
+import XCTest
+@testable import _9_23_swift_02
+
+class _9_23_swift_02Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
